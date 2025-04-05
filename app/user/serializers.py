a0587715146284487230d9b74bdb44cd377b2b26
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2025-04-05 16:22:12 +0100

    [Feat] Add token authentication functionality
    - Create serializers class to create token upon successful user authentication.
    - Create token view class to handle token creation serialization.

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -1,8 +1,11 @@
 """
 Serializer for the USER API View.
 """
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate
 from rest_framework import serializers
+
+from django.utils.translation import gettext as _
+
 
 class UserSerializer(serializers.ModelSerializer):
     """Serializer for the user object."""
@@ -21,3 +24,29 @@ class UserSerializer(serializers.ModelSerializer):
         def create(self, validated_data):
             """Create and return user object with encrypted data."""
             return get_user_model().objects.create_user(**validated_data)
+
+class AuthTokenSerializer(serializers.Serializer):
+    """Serializer for user auth token."""
+    email = serializers.EmailField()
+    password = serializers.CharField(
+        style = {'input_type': 'password'},
+        trim_whitespace = False
+    )
+
+    def validate(self, attrs):
+        """Validate and authenticate the user data."""
+        email = attrs.get('email')
+        password = attrs.get('password')
+
+        user = authenticate(
+            request = self.context.get('request'),
+            username = email,
+            password = password
+        )
+
+        if not user:
+            msg = _("Unable to authenticate with provided credentials.")
+            raise serializers.ValidationError(msg, code='authorization')
+
+        attrs['user'] = user
+        return attrs
