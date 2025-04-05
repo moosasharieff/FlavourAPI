26e53c44c667ca327a760e728ec5b6200a3bf6cb
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2025-04-05 18:13:53 +0100

    [Feat] Add update functionality to user page.
    - Add 'update' method in 'UserSerializer' to update user data such as name, password, email.
    - Add new view class to manage updating of data.

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -25,6 +25,19 @@ class UserSerializer(serializers.ModelSerializer):
             """Create and return user object with encrypted data."""
             return get_user_model().objects.create_user(**validated_data)
 
+        def update(self, instance, validated_data):
+            """Update user data."""
+            password = validated_data.pop('password', None)
+            user = super().update(instance, validated_data)
+
+            if password:
+                user.set_password(password)
+                user.save()
+
+            return user
+
+
+
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for user auth token."""
     email = serializers.EmailField()
