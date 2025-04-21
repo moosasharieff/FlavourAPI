4a6f9ae9b1391e53dd07ad4e2642c13378e0a3c5
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2025-04-21 10:58:31 +0100

    [Test] Ddd test cases to validate flavour retrieval for authenticated users
    - Added tests for retrieving all flavours for an authenticated user
    - Ensured that only flavours created by the authenticated user are returned

--- app/flavour/tests/test_flavour_api.py
+++ app/flavour/tests/test_flavour_api.py
@@ -54,3 +54,46 @@ class PublicFlavourAPITests(TestCase):
         # Assertions
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
+
+class PrivateFlavourAPITests(TestCase):
+    """Test cases of Flavour API for authorized user."""
+    def setUp(self):
+        """Setting up testing environrment."""
+        self.user = create_user()
+        self.client = APIClient()
+        self.client.force_authenticate(self.user)
+
+    def test_retrieve_flovours(self):
+        """Test retrieve reciepes."""
+        create_flavour(user=self.user)
+        create_flavour(user=self.user)
+
+        response = self.client.get(FLAVOUR_URL)
+        flavour = Flavour.objects.all().order_by('-id')
+        serializer = FlavourSerializer(flavour, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+    def test_retrive_flavour_for_user(self):
+        """Test retrieving flavour for specific user."""
+        other_user_creds = {
+            'email': 'otherTester@testing.com',
+            'password': 'testPassword'
+        }
+
+        other_user = create_user(**other_user_creds)
+
+        create_flavour(self.user)
+        create_flavour(other_user)
+
+        response = self.client.get(FLAVOUR_URL)
+
+        db_flavour_data = Flavour.objects.filter(user=self.user)
+        serializer = FlavourSerializer(db_flavour_data, many=True)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
