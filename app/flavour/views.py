3d4eaae53b662cec5237ecf4a81ab40ff60ba320
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2025-04-21 11:44:40 +0100

    [Feat] Implement FlavourViewSet for authenticated user-specific data access
    - Add ModelViewSet for Flavour model
    - Restricted access to authentiated users using TokenAuthentication
    - Filtered queryset to return only flavour created by the requesting user

--- app/flavour/views.py
+++ app/flavour/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Flavour
+
+from .serializers import FlavourSerializer
+
+
+class FlavourViewSet(viewsets.ModelViewSet):
+    """View for managing Flavour API."""
+    serializer_class = FlavourSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    queryset = Flavour.objects.all()
+
+    def get_queryset(self):
+        """Return flavour query only for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
